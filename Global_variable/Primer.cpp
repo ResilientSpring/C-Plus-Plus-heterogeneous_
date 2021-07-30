6ac829e51f26a00d61fc66b97f06d66a624e5982
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-07-30 17:36:56 +0800

    Committed 2021/7/30

---
+++ Global_variable/Primer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This program will help you multiply two numbers" << endl;
+
+	cout << "Enter the first number: ";
+	int first_number;
+	cin >> first_number;
+
+	cout << "Enter the second number: ";
+	int secondNumber = 0;
+	cin >> secondNumber;
+
+	cout << first_number << ' x ' << secondNumber << ' = ' << first_number * secondNumber;
+
+	return 0;
+}
