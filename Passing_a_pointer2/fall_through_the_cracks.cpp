e10156fc4e2c02f8953f96677adf756f835eca63
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-07 15:02:41 +0800

    Committed 2021/08/07

--- Passing_a_pointer2/fall_through_the_cracks.cpp
+++ Passing_a_pointer2/fall_through_the_cracks.cpp
@@ -2,22 +2,16 @@
 #include <iostream>
 using namespace std;
 
-void f(int *j);
+void f(int* j);
 
 int main() {
-
 	int i;
-	int* p;
 
-	p = &i;
-
-	f(p); // pass a pointer.
-
+	f(&i);  // No need for p. The address of i is passed directly. 
 	cout << "i = " << i;
-
+	return 0;
 }
 
-// f() receives a pointer to an int. 
-void f(int *j) {
+void f(int* j) {
 	*j = 100; // var pointed to by j is assigned 100.
 }
